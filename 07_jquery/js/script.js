2ac41eb2f71ebece352f9333d8e65979c6c72ac9
Marie Otsuka <marieotsuka@users.noreply.github.com>
2018-01-16 17:32:50 -0500

    jquery demo updates

--- 07_jquery/js/script.js
+++ 07_jquery/js/script.js
@@ -1,13 +1,21 @@
+console.log('Hello from external script');
 $(document).ready(function(){
 
 	$('.button').click(function(){
 		console.log('click detected');
-		$('h1').toggleClass('reveal');
+		$('h1').toggle();
 	});
 
-	$(".element").click(function(){
-		console.log("clicking element");
+	$('.element').click(function(){
+		console.log('element clicked');
 		$(this).toggleClass('play');
+	});
+
+	$('h2').click(function(){
+		var sample = $(this).text();
+		console.log(sample);
+		$('.a').text(sample).toggle();
+		$('.b').toggle();
 	});
 
 });
